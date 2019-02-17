03f4545232a34f03af4656d76b12e3d58a001d59
Trevor Xander <trevorcolexander@gmail.com>
2019-02-17 00:34:35 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+setup(
+    name='TextPredictor',
+    version='0.1',
+    url='',
+    license='GNU Affero General Public License v3.0',
+    author='Trevor Xander',
+    author_email='trevorcolexander@gmail.com',
+    long_description=read('README.md'),
+    install_requires=[],
+    py_modules=['text_predictor'],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'textpredict = predictor:predict_text'
+        ]
+    }
+)

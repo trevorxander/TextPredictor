379ade9fa3b2391c5dee7f1439c53bbb19472dae
Trevor Xander <trevorcolexander@gmail.com>
2019-02-28 12:53:41 -0500

    Add pad for ngram higher than 1

--- text_predictor/preprocess.py
+++ text_predictor/preprocess.py
@@ -1,10 +1,16 @@
-def pad(sentence):
-    return '<s> ' + sentence + ' </s>'
 
+def pad(sentence, ngram, start_tag, end_tag):
+    processed = '{pad_start} {sentence} {pad_end}'
+    pad_start = start_tag
+    for pad_count in range(ngram):
+        pad_start += ' ' + start_tag
+
+    return processed.format(pad_start=pad_start, pad_end=end_tag, sentence=sentence)
 
 def to_lowercase(sentence: str):
     return sentence.lower()
 
-
 def remove_newline(sentence):
     return sentence.rstrip('\n\r')
+
+prepocess_funcs = [remove_newline, to_lowercase]
